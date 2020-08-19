c2b064bdb124dcd0a281cfa521d43f1095646c5d
codegangsk <kjhj0604@gmail.com>
2020-08-19 16:24:04 +0900

    first commit

---
+++ CommonInputControls(2.9  Controls in Action project)/ViewController+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController+Extension.swift
+//  CommonInputControls(2.9  Controls in Action project)
+//
+//  Created by Sophie Kim on 2020/08/07.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import Foundation
+
+extension ViewController {
+    func printHi() {
+        print("Hi from a different file")
+    }
+}
+
--- CommonInputControls(2.9  Controls in Action project)/ViewController.swift
+++ CommonInputControls(2.9  Controls in Action project)/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var toggle: UISwitch!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
     }
-
-
 }
 
+extension ViewController {
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Button was tapped!")
+        
+        if toggle.isOn {
+            print("The switch is on!")
+        } else {
+            print("The switch is off.")
+        }
+        print("The slider is set to \(slider.value)")
+    }
+    
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        guard sender.isOn else {
+            print("The switch is off.")
+            return
+        }
+        
+        print("The switch is on!")
+        
+//        if sender.isOn {
+//            print("The switch is on!")
+//        } else {
+//            print("The switch is off.")
+//        }
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func keyboardReturnKeyTapped(_ sender: UITextField) {
+        sender.resignFirstResponder()
+        
+        guard let text = sender.text else { return }
+        
+        printHi()
+        print(text)
+    }
+    
+    
+    @IBAction func textChanged(_ sender: UITextField) {
+        guard let text = sender.text else { return }
+        
+        print(text)
+    }
+    
+    @IBAction func onDidTapGesture(_ sender: UITapGestureRecognizer) {
+        print("onDidTapGesture")
+    }
+}
